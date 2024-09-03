3e3049d531ec88133df745d960c4cfe67e125027
Maya <mayra.marulanda@hotmail.com>
2024-09-04 10:25:11 +1030

    Avance en módulo de hoteles

---
+++ hotel.py
@@ -0,0 +1,39 @@
+
+
+hoteles = { 
+     '1': { 
+       "nombre": 'Hotel Marulanda',
+        'habitaciones':
+          { '202': {'tipo': 'individual', 'precio':60.000,'Disponible': True},
+            '204': {'tipo': 'doble', 'precio':180.000,'Disponible': False},
+            '206': {'tipo': 'suite', 'precio':210.000,'Disponible': True},
+          }
+     },
+
+      '2': {
+          "nombre": 'Hotel Astrudillo',
+          'habitaciones':{
+            '301': {'tipo': 'individual', 'precio':90.000,'Disponible': True},
+            '302': {'tipo': 'doble', 'precio':170.000,'Disponible': True},
+            '303': {'tipo': 'suite', 'precio':210.000,'Disponible': False},
+            },
+
+       '3': {
+          "nombre": 'Hotel Orquideas',
+          'habitaciones':{
+            '401': {'tipo': 'individual', 'precio':80.000,'Disponible': True},
+            '402': {'tipo': 'doble', 'precio':140.000,'Disponible': True},
+            '403': {'tipo': 'suite', 'precio':250.000,'Disponible': True},
+            },
+    }
+
+  }
+     
+}
+
+
+
+
+
+
+
